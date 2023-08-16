969510cedc111532834efee46bff7e64e210dbf7
Marshall Couch <marshall.couch@gmail.com>
2023-08-15 20:38:52 -0400

    added the ability to scroll through list categories

--- dont_say_the_word/dont_say_the_word.gd
+++ dont_say_the_word/dont_say_the_word.gd
@@ -4,12 +4,20 @@ extends Node2D
 
 var team1_score:int
 var team2_score:int
-var lists: Object
+var lists: Dictionary
+var used_items: Dictionary
+var is_playing: bool = false
+var list_selected:int = -1
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	team1_score = 0
 	team2_score = 0
+	load_lists()
+	next_category()
+	
 
 
 func _on_team_one_up_button_down() -> void:
@@ -30,12 +38,19 @@ func _on_team_two_down_button_down() -> void:
 
 func load_lists() -> void:
 	var file = File.new()
-	file.open("res://assets/gamelists.tres",File.READ)
+	file.open("res://gamelists.tres",File.READ)
 	var gamelists = file.get_as_text()
 	gamelists = gamelists.replace("[gd_resource type=\"Resource\" format=2]","").replace("[resource]","")
 	lists = JSON.parse(gamelists).result
-	
 
+
+func next_category() -> void: 
+	if list_selected == lists["Lists_Count"]-1:
+		list_selected = 0
+	else:
+		list_selected += 1
+	
+	$category_label.text = "Category: " + lists["Lists"][list_selected]
 	
 func update_team1label():
 	$team_one_score.text = "Team 1 Score: " + str(team1_score)
@@ -45,3 +60,7 @@ func update_team2label():
 
 
 
+
+
+func _on_next_category_button_button_down() -> void:
+	next_category()
