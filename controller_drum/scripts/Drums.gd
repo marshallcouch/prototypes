8f6a37f512acd9a7136b8b1cf7ab699d150a9ce2
Marshall Couch <marshall.couch@gmail.com>
2024-03-09 16:45:40 -0500

    added drumming with controller.   feels really weird.  I'll call this an L.

--- controller_drum/scripts/Drums.gd
+++ controller_drum/scripts/Drums.gd
@@ -9,3 +9,25 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _input(event):
+	if(event.is_action_pressed("Kick")):
+		$Kick.play()
+	if(event.is_action_pressed("SnareLeft")):
+		$SnareLeft.play()
+	if(event.is_action_pressed("SnareRight")):
+		$SnareRight.play()
+	if(event.is_action_pressed("Crash")):
+		$Crash.play()
+	if(event.is_action_pressed("Splash")):
+		$Splash.play()
+	if(event.is_action_pressed("FloorTom")):
+		$FloorTom.play()
+	if(event.is_action_pressed("MidTom")):
+		$MidTom.play()
+	if(event.is_action_pressed("Hat")):
+		if(Input.is_action_pressed("HatPedal")):
+			$HatClosed.play()
+		else:
+			$HatOpen.play()
