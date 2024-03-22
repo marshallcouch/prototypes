288a244011f20d51c2ddca5d3a502e976fb09474
Marshall Couch <marshall.couch@gmail.com>
2024-03-21 20:07:45 -0400

    added some typing.  I should get in the habit of that

--- Thumb_Arena/src/controls.gd
+++ Thumb_Arena/src/controls.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-const SPEED = 300
-const DEAD_ZONE = .15
-const MAX_ANALOG = .85
+const SPEED:int = 300
+const DEAD_ZONE:float = .15
+const MAX_ANALOG:float = .85
 @onready var players_node = $Players
 
 # Called when the node enters the scene tree for the first time.
@@ -17,16 +17,16 @@ func _process(delta):
 
 
 func player_movement(player_number: int = 0):
-	var x = 0
-	var y = 0
+	var x:float = 0
+	var y:float = 0
 	if player_number == 0:
 		x = Input.get_joy_axis(0,JOY_AXIS_LEFT_X)
 		y = Input.get_joy_axis(0,JOY_AXIS_LEFT_Y)
 	if player_number == 1:
 		x = Input.get_joy_axis(0,JOY_AXIS_RIGHT_X)
 		y = Input.get_joy_axis(0,JOY_AXIS_RIGHT_Y)
-	var d = sqrt(x**2 + y**2)
-	var v = Vector2(0,0)
+	var d:float = sqrt(x**2 + y**2)
+	var v:Vector2 = Vector2(0,0)
 	if d < DEAD_ZONE:
 		v = Vector2(0,0)
 	elif d > MAX_ANALOG:
