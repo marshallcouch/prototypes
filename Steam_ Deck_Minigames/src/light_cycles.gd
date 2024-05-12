cae9880492d61f07a2020005ad29401f2aac259f
Marshall Couch <marshall.couch@gmail.com>
2024-05-12 14:23:48 -0400

    progress

--- Steam_ Deck_Minigames/src/light_cycles.gd
+++ Steam_ Deck_Minigames/src/light_cycles.gd
@@ -3,12 +3,13 @@ extends Node
 @onready var player_one_shape = $Players/PlayerOne/PlayerShape
 @onready var player_two = $Players/PlayerTwo
 @onready var player_two_shape = $Players/PlayerTwo/PlayerShape
+@onready var player_one_score_label = $PlayerOneScore
+@onready var player_two_score_label = $PlayerTwoScore
 @onready var tails = $Tails
 var tail = preload("res://scenes/light_cycles/tail_node.tscn")
 
 var last_tails = [Vector2(0,0),Vector2(0,0)]
 var player_speed = 300
-var player_speed_delta = 0
 const DEAD_ZONE:float = .15
 const MAX_ANALOG:float = .85
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +21,20 @@ func _physics_process(delta):
 	player_movement(player_one, player_one_shape, 0)
 	player_movement(player_two, player_two_shape, 1)
 
+func reset():
+	new_round()
+	player_one_score_label.text = "0"
+	player_two_score_label.text = "0"
+	
 
+func new_round():
+	player_one.position = Vector2(20,400)
+	player_two.position = Vector2(1260,400)
+	player_one.velocity = Vector2(0,0)
+	player_two.velocity = Vector2(0,0)
+	for tail in tails.get_children():
+		tail.queue_free()
+	resetting = false
 
 func player_movement(player: Node,shape:Node,player_number:int):
 	var x:float = 0
@@ -33,23 +47,37 @@ func player_movement(player: Node,shape:Node,player_number:int):
 	if stick == 1:
 		x = Input.get_joy_axis(controller,JOY_AXIS_RIGHT_X)
 		y = Input.get_joy_axis(controller,JOY_AXIS_RIGHT_Y)
-	var old_velocity = player.velocity
-	if abs(x) > abs(y) and abs(x) > .5:
-		player.velocity = Vector2(x/abs(x),0) * (player_speed+ player_speed_delta)
-		shape.rotation = 0
-	elif abs(x) < abs(y) and abs(y) > .5:
-		player.velocity = Vector2(0,y/abs(y)) * (player_speed + player_speed_delta)
-		shape.rotation = PI/2
 	if player.velocity == Vector2(0,0):
 		if player_number == 0:
-			player.velocity = Vector2(1,0) * (player_speed+ player_speed_delta)
+			x=1
+			player.velocity = Vector2(1,0)
 		else:
-			player.velocity = Vector2(-1,0) * (player_speed+ player_speed_delta)
-	if distance(last_tails[player_number],player.position) > 8 or old_velocity != player.velocity:
+			x=-1
+			player.velocity = Vector2(-1,0)
+			
+	var old_velocity = player.velocity
+	var old_rotation = shape.rotation
+	if abs(x) > abs(y) and abs(x) > .5:
+		player.velocity = Vector2(x/abs(x),0) * (player_speed)
+		if x > 0 :
+			shape.rotation = 0
+		else: 
+			shape.rotation = PI
+	elif abs(x) < abs(y) and abs(y) > .5:
+		player.velocity = Vector2(0,y/abs(y)) * (player_speed )
+		if y < 0:
+			shape.rotation = PI*1.5
+		else:
+			shape.rotation = PI/2
+	if player.velocity == -old_velocity:
+		player.velocity = old_velocity
+		shape.rotation = old_rotation
+		
+	if distance(last_tails[player_number],player.position) > 4 or old_velocity != player.velocity:
 		var new_tail = tail.instantiate()
-		tails.add_child(new_tail)
 		new_tail.position = player.position
-		last_tails[player_number] = player.position
+		last_tails[player_number] = new_tail.position
+		tails.add_child(new_tail)
 	player.move_and_slide()
 	
 func distance(pos1, pos2) -> float:
@@ -65,3 +93,20 @@ func _x_y_movement(x:float, y:float) -> Vector2:
 	else:
 		v = Vector2(x,y)
 	return v 
+
+var resetting: bool = false
+func _on_player_one_area_area_entered(area):
+	if resetting: 
+		return
+	resetting = true
+	new_round()
+	player_two_score_label.text = str(int(player_two_score_label.text)+1)
+
+func _on_player_two_area_area_entered(area):
+	if resetting: 
+		return
+	resetting = true
+	new_round()
+	player_one_score_label.text = str(int(player_one_score_label.text)+1)
+	
+	
