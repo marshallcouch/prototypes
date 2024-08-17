34793091b68e582419e51c56a555b6faa35d611c
Marshall Couch <marshall.couch@gmail.com>
2024-08-16 22:09:02 -0400

    updated the cheat sheet

--- GodotCheatSheet.gd
+++ GodotCheatSheet.gd
@@ -1,2 +1,21 @@
 #Kill the Game
 get_tree().quit()
+
+
+#preloads
+var my_preload = preload("res://scenes/boss.tscn")
+my_preload.instantiate()
+
+#onready variables for nodes
+@onready var soundtrack = $MusicPlayer
+
+
+
+
+
+
+Shaders:
+//get the entire screen
+uniform sampler2D screen_texture: hint_screen_texture;
+
+material.set_shader_parameter("uniform_value", 123)
