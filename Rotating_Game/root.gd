b2aa7b9bc4982d31cdc51a3deaeca49b0c3c7881
Marshall Couch <marshall.couch@gmail.com>
2024-12-01 22:05:50 -0500

    handling gravity for rotation

--- Rotating_Game/root.gd
+++ Rotating_Game/root.gd
@@ -11,9 +11,15 @@ func _process(delta):
 
 
 func _on_button_button_up():
+	var new_gravity:Vector2 
 	if abs($"Game Area".position.y) < 0.1:
 		$"Game Area".rotate(deg_to_rad(-90))
 		$"Game Area".position.y = 1560
+		new_gravity = Vector2(1,0)
+		$"Game Area/RigidBody2D".linear_velocity = $"Game Area/RigidBody2D".linear_velocity.rotated(deg_to_rad(-90))
 	else:
 		$"Game Area".rotation = 0.0
 		$"Game Area".position.y = 0.0
+		new_gravity = Vector2(0,1)
+		$"Game Area/RigidBody2D".linear_velocity = $"Game Area/RigidBody2D".linear_velocity.rotated(deg_to_rad(90))
+	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, new_gravity)
